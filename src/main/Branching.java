aa6ed7cb6d1ffacbef49ccb2b0db42fc11fba0d2
parbati1 <shresthaparbati748@gmail.com>
2021-03-12 14:16:05 -0600

    branching added

---
+++ src/main/Branching.java
@@ -0,0 +1,8 @@
+package main;
+
+public enum Branching {
+    MASTER,
+    MAIN,
+    BRANCH1,
+    BRANCH2
+}
---
+++ src/main/Run.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.text.AbstractDocument;
+
+public class Run {
+        public static void main(String[] args){
+
+            main.Branching master= main.Branching.MASTER;
+            System.out.println(master);
+            //Loop in Enum
+            for (Branching branch:Branching.values()){
+                System.out.println(Branch);
+            }
+        }
+    }
+
